d33545a281d0373ebacac8f5195c46c9646224d6
Brady Douthit <brady.douthit99@gmail.com>
2019-09-18 14:18:02 -0400

    styled the page

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 $.getJSON("/articles", function (data) {    
     data.forEach(Element => {
         $('#articles').append(
-            `<h2>${Element.title}</h2><a href="${Element.link}" target="_blank">The original article</a><p>${Element.paragraph}</p>`
+            `<div class="article"><h2>${Element.title}</h2><a href="${Element.link}" target="_blank">The original article</a><p>${Element.paragraph}</p></div>`
         );
     });
 });
