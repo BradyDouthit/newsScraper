02e0da898cba3ca605230ca634541483adce155d
Brady Douthit <brady.douthit99@gmail.com>
2019-09-20 16:35:46 -0400

    fixed an id mismatch with the post

--- public/app.js
+++ public/app.js
@@ -1,13 +1,13 @@
 $.getJSON("/articles", function (data) {
     data.forEach(Element => {
-        $('#articles').append(
+        $('#articles').prepend(
             `<div class="article">
                 <h2>${Element.title}</h2>
                 <h4 id="summary-h4">Click a summary to view the full article</h4>
                 <a id="p-link" href="${Element.link}" target="_blank"
                 <p>${Element.paragraph}</p>
                 </a>
-                <input data-id="${Element._id}" type="text" id="comment" placeholder="Wow! Very cool!">
+                <input data-id="${Element._id}" class="comment-field" type="text" id="comment${Element._id}" placeholder="Wow! Very cool!">
                 <button data-id="${Element._id}" id="comment-button">Add a comment</button>
             </div>`
         );
@@ -18,7 +18,7 @@ $(document).on('click', '#comment-button', function (event) {
     event.preventDefault();
 
     let thisId = $(this).attr("data-id");
-    let comment = $('#comment').val();
+    let comment = $('#comment' + thisId).val();
 
     console.log(thisId)
     $.ajax({
@@ -29,8 +29,14 @@ $(document).on('click', '#comment-button', function (event) {
         }
     })
     .then(function(data) {
-
-        console.log(data)
+        $.get('/articles/' + thisId, function(articles) {
+            console.log(articles)
+            articles.comment.forEach(Element => {
+                $('#comments-div').append(`
+                <div class="comments">${Element.body}</div>
+                `)
+            })
+        })
         
     })
 
